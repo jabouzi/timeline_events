6bce9cc7d2cd3ad207c4aba3d8c7c4fef74ec1d7
Skander Jabouzi <jabouzi@gmail.com>
2015-10-20 17:42:44 +0000

    add compagne module

--- application/modules/campagne/controllers/campagne.php
+++ application/modules/campagne/controllers/campagne.php
@@ -1,6 +1,6 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Dashboard extends MX_Controller
+class Campagne extends MX_Controller
 {
 	
 	function __construct()
