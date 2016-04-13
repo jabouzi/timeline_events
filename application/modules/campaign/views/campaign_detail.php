7bacf169be13410eb75f37f8044431e7d2f8e203
Skander Jabouzi <jabouzi@gmail.com>
2016-04-13 13:17:06 +0000

    detail print

--- application/modules/campaign/views/campaign_detail.php
+++ application/modules/campaign/views/campaign_detail.php
@@ -11,7 +11,7 @@
 			<a href="<?php echo site_url('campaign'); ?>" class="btn-icon-on-dark on-white">
 			<?= lang('campaign.return_btn')?>
 			</a>
-			<a target="_blank" href="<?php echo site_url('campaign/campaign_detail_print/'.$campaign_id.'/'.$this->session->userdata('current_site_lang')); ?>" class="btn-icon-on-dark on-white">
+			<a style="cursor:pointer;" onclick="frames['frame'].print()" class="btn-icon-on-dark on-white">
 			<svg class="icon-svg icon-imprimerRouge" width="36" height="32" viewBox="0 0 36 32" x="720">
 				<path fill="#e41b13" d="M11.273 18.232H25.09v1.09H11.274v-1.09zm0 4H25.09v1.092H11.274v-1.092zm0 3.998H25.09v1.093H11.274V26.23z"/>
 				<path fill="#e41b13" d="M36.364 7.884h-7.637V.25H7.637v7.634H.002v17.453h7.35v6.413H28.73v-6.413h7.637V7.883zM9.09 1.704h18.183v6.18H9.09v-6.18zm18.205 28.593H9.037V14.762h18.258v15.535z"/>
@@ -76,4 +76,5 @@
 			</div>
 		</div>
 	</div>
+	<iframe src="<?php echo site_url('campaign/campaign_detail_print/'.$campaign_id.'/'.$this->session->userdata('current_site_lang')); ?>" style="position:absolute;left:-999999px;" frameBorder="0" width="100%" height="100%" name="frame"></iframe>
 </section>
